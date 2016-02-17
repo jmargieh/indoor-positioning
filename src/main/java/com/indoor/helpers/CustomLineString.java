d116c1e19989abee4e746f23604dbc8d4e911613
Jawad Margieh <J.margieh@gmail.com>
2016-02-17 19:20:20 +0200

    GridSquare+CustomLineString

--- src/main/java/com/indoor/helpers/CustomLineString.java
+++ src/main/java/com/indoor/helpers/CustomLineString.java
@@ -1,5 +1,15 @@
 package main.java.com.indoor.helpers;
+import com.vividsolutions.jts.geom.Geometry;
 
 public class CustomLineString {
+	
+	private Geometry line;
+	private double rate;
+	
+	
+	public CustomLineString(Geometry line, double rate) {
+		this.line = line;
+		this.rate = rate;
+	}
 
 }
--- src/main/java/com/indoor/helpers/GridSquare.java
+++ src/main/java/com/indoor/helpers/GridSquare.java
@@ -10,6 +10,48 @@ public class GridSquare {
 	private String id;
 	private SimpleFeature square;
 	private double rate;
-	private List<CustomLineString> customLineStringArray = new ArrayList<>();
+	private List<CustomLineString> customLineStringArray;
+	
+	public GridSquare(String id, SimpleFeature square, double rate, List<CustomLineString> LineArray){
+		this.id = id;
+		this.square = square;
+		this.rate = rate;
+		this.customLineStringArray = new ArrayList<CustomLineString>();
+		for(CustomLineString line : LineArray)
+		{
+			this.customLineStringArray.add(line);
+		}
+	}// end c'tor
+	
+	public GridSquare(String id,SimpleFeature square,double rate) {
+		this.id = id;
+		this.square = square;
+		this.customLineStringArray = new ArrayList<CustomLineString>();
+		this.rate = rate;
+	}
+	public String getId(){
+		return this.id;
+	}
+	
+	public SimpleFeature getSquare(){
+		return this.square;
+	}
+	
+	public double getRate(){
+		return this.rate;
+	}
+	
+	public List<CustomLineString> getLineStringArray(){
+		return this.customLineStringArray;
+	}
+	
+	public void addLineString(CustomLineString line){
+		this.customLineStringArray.add(line);
+	}
+	
+	public void setRate(double rate){
+		this.rate = rate;
+	}
+	
+	}
 
-}
