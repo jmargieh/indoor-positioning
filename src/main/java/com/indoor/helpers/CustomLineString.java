d6d1095679a57e8c97f46bbb592c7905f2d7f210
Jawad Margieh <J.margieh@gmail.com>
2016-02-18 15:36:14 +0200

    added rate with enum

--- src/main/java/com/indoor/helpers/CustomLineString.java
+++ src/main/java/com/indoor/helpers/CustomLineString.java
@@ -1,15 +1,75 @@
 package main.java.com.indoor.helpers;
 import com.vividsolutions.jts.geom.Geometry;
 
+
 public class CustomLineString {
 	
 	private Geometry line;
 	private double rate;
+	public enum Direction {
+	    UP,
+	    RIGHT,
+	    DOWN,
+	    LEFT
+	}
+	private Direction direction;
 	
-	
-	public CustomLineString(Geometry line, double rate) {
+	public CustomLineString(Geometry line, double rate, String direction) {
+		this.setLine(line);
+		this.setRate(rate);
+		
+		switch (direction) {
+		case "UP":
+			this.setDirection(Direction.UP);
+			break;
+ 
+		case "RIGHT":
+			this.setDirection(Direction.RIGHT);
+			break;
+ 
+		case "DOWN":
+			this.setDirection(Direction.DOWN);
+			break;
+		case "LEFT":
+			this.setDirection(Direction.LEFT);
+			break;
+ 
+		default:
+			System.err.println("Something's wrong with CustomLineString Direction");
+			break;
+		}
+	}
+
+
+	public Geometry getLine() {
+		return this.line;
+	}
+
+
+	public void setLine(Geometry line) {
 		this.line = line;
+	}
+
+
+	public double getRate() {
+		return rate;
+	}
+
+
+	public void setRate(double rate) {
 		this.rate = rate;
 	}
+
+
+	public Direction getDirection() {
+		return this.direction;
+	}
+
+
+	public void setDirection(Direction direction) {
+		this.direction = direction;
+	}
+	
+	
 
 }
