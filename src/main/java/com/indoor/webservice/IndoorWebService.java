0096bea2d4fc8c64b83ff71b34082b96ad9c99b3
Jawad Margieh <J.margieh@gmail.com>
2016-02-18 15:37:05 +0200

    added Grid Square poularity & linestring popularity

--- src/main/java/com/indoor/webservice/IndoorWebService.java
+++ src/main/java/com/indoor/webservice/IndoorWebService.java
@@ -15,6 +15,7 @@ import javax.ws.rs.core.Response;
 import main.java.com.indoor.helpers.CustomComparator;
 import main.java.com.indoor.helpers.CustomLineString;
 import main.java.com.indoor.helpers.GridSquare;
+import main.java.com.indoor.helpers.CustomLineString.Direction;
 
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
@@ -29,7 +30,6 @@ import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.opengis.feature.simple.SimpleFeature;
 import org.springframework.stereotype.Component;
 
-import com.vividsolutions.jts.algorithm.CentroidPoint;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
@@ -62,9 +62,8 @@ public class IndoorWebService {
 	private HashMap<String, List<SimpleFeature>> deviceMap = new HashMap<String, List<SimpleFeature>>();
 	// HashMap with key:deviceid & LineString: the device path
 	private HashMap<String, LineString> devicePathMap = new HashMap<String, LineString>();
-	
-	//private ArrayList<? extends Geometry> pointsArray;
-	//private ArrayList<? extends Geometry> obstaclesArray;
+	// GridMap Matrix Reference : http://docs.geotools.org/latest/userguide/extension/grid.html
+	private GridSquare gridMapMatrix[][];
 
 	// close Input Streams
 	private void closeInputStreams() throws IOException {
@@ -116,7 +115,7 @@ public class IndoorWebService {
 				SimpleFeature simplefeature = pIterator.next();
 				if (obstacle.contains((Geometry)simplefeature.getAttribute("geometry"))) {
 					// remove the point from pointsArray
-					//pIterator.remove();
+					pIterator.remove();
 					// add the problematic point to pointsInObstaclesArray
 					pointsInObstaclesArray.add(simplefeature);
 				}
@@ -127,21 +126,11 @@ public class IndoorWebService {
 		putPointsIntoDeviceHashMap();
 		createDevicesPaths();
 		createGridMap();
+		deleteCustomLineStringCrossingObstacle();
+		updateRateGridSquares();
+		updateRateCustomLineInGrid();
 		
-		
-		String Res="";
-		for (LineString line : this.devicePathMap.values()) {
-		    boolean flag = isPathCrossingObstacle(line);
-		    if(flag){
-		    	Res += "true | ";
-		    }else{
-		    	Res += "false |";
-		    }
-		    
-		}
-		
-		
-		return Response.status(200).entity(Res + "   -----If reached here we succeed!").build();
+		return Response.status(200).entity("   -----If reached here we succeed!").build();
 
 	}
 	
@@ -205,7 +194,64 @@ public class IndoorWebService {
 			LineString line = geometryFactory.createLineString(coords);
 			this.devicePathMap.put(list.get(0).getID(), line);
 		}
-		
+	}
+	private void updateRateGridSquares(){
+		for(int i=0;i<this.gridMapMatrix.length;i++){
+			for(int j=0;j<this.gridMapMatrix[i].length; j++){
+				for (Iterator<SimpleFeature> pIterator = this.pointsArray.iterator(); pIterator.hasNext();) {
+					SimpleFeature simplefeature = pIterator.next();
+					if( ((Geometry)(gridMapMatrix[i][j].getSquare().getAttribute("element"))).contains((Geometry)simplefeature.getAttribute("geometry")) ){
+						gridMapMatrix[i][j].setRate(gridMapMatrix[i][j].getRate()+1);
+					}
+				}
+
+			}
+		}
+	}
+	
+	private void updateRateCustomLineInGrid(){
+		for(int i=0; i<this.gridMapMatrix.length; i++){
+			for(int j=0;j<this.gridMapMatrix[i].length; j++){
+				// we need heuristics, meanwhile it's 1
+				List<CustomLineString> customLineStringArray = this.gridMapMatrix[i][j].getLineStringArray();
+				for (Iterator<CustomLineString> cIterator = customLineStringArray.iterator(); cIterator.hasNext();){
+					CustomLineString cls = cIterator.next();
+					if(cls.getDirection() == Direction.UP){
+						cls.setRate(this.gridMapMatrix[i-1][j].getRate());
+					}
+					if(cls.getDirection() == Direction.RIGHT){
+						cls.setRate(this.gridMapMatrix[i][j+1].getRate());
+					}
+					if(cls.getDirection() == Direction.DOWN){
+						cls.setRate(this.gridMapMatrix[i+1][j].getRate());
+					}
+					if(cls.getDirection() == Direction.LEFT){
+						cls.setRate(this.gridMapMatrix[i][j-1].getRate());
+					}
+					
+				}
+			}
+		}
+	}
+	
+	private void deleteCustomLineStringCrossingObstacle() {
+		boolean isListChanged;
+		for (int i = 0 ; i < this.gridMapMatrix.length ; i++) {
+			for(int j=0 ; j<this.gridMapMatrix[i].length; j++ ){
+				isListChanged = false;
+				List<CustomLineString> customLineStringArray = this.gridMapMatrix[i][j].getLineStringArray();
+				for (Iterator<CustomLineString> cIterator = customLineStringArray.iterator(); cIterator.hasNext();) {
+					Geometry line = (Geometry)cIterator.next().getLine();
+					if(isPathCrossingObstacle(line)){
+						cIterator.remove();
+						isListChanged = true;
+					}
+				}
+				if(isListChanged){
+					this.gridMapMatrix[i][j].setLineStringArray(customLineStringArray);
+				}
+			}
+		}
 	}
 	
 	// checks if a path/Stringline is crossing an obstacle.
@@ -246,14 +292,14 @@ public class IndoorWebService {
 			}
 			
 			// if reached here that means everything is OK with the collection size and Matrix to be allocated is fine
-			GridSquare gridMapMatrix[][] = new GridSquare[(int)matrixRows][(int)matrixCols];
+			this.gridMapMatrix = new GridSquare[(int)matrixRows][(int)matrixCols];
 			SimpleFeatureIterator iterator=collection.features();
 			try {
 				
 				for(int i=(int) (matrixRows-1) ; i>=0 ; i--){
 					for(int j=0 ; j<(int) (matrixCols) && iterator.hasNext() ; j++){
 						SimpleFeature feature = iterator.next();
-						gridMapMatrix[i][j] = new GridSquare(Integer.toString(i)+"-"+Integer.toString(j), feature,0);
+						this.gridMapMatrix[i][j] = new GridSquare(Integer.toString(i)+"-"+Integer.toString(j), feature,0);
 					}
 				}
 			}finally {
@@ -263,39 +309,39 @@ public class IndoorWebService {
 				for(int i=(int) (matrixRows-1) ; i>=0 ; i--){
 					for(int j=0 ; j<(int) (matrixCols) ; j++){
 						
-						Geometry g = (Geometry)gridMapMatrix[i][j].getSquare().getAttribute("element");
+						Geometry g = (Geometry)this.gridMapMatrix[i][j].getSquare().getAttribute("element");
 						Point centroidPoint = g.getCentroid();
 						Geometry line = null;
 						
 						if(j > 0) // if not most left column then compute lineString to left
 						{
-							Geometry gLeft = (Geometry)gridMapMatrix[i][j-1].getSquare().getAttribute("element");
+							Geometry gLeft = (Geometry)this.gridMapMatrix[i][j-1].getSquare().getAttribute("element");
 							Point centroidPointLeft = gLeft.getCentroid();
 							line = createLineString(centroidPoint, centroidPointLeft);
-							gridMapMatrix[i][j].addLineString(new CustomLineString(line, 0));
+							this.gridMapMatrix[i][j].addLineString(new CustomLineString(line, 0,"LEFT"));
 						}
 						if(j < (int) (matrixCols - 1)) // if not most right column then compute lineString to right
 						{
-							Geometry gRight = (Geometry)gridMapMatrix[i][j+1].getSquare().getAttribute("element");
+							Geometry gRight = (Geometry)this.gridMapMatrix[i][j+1].getSquare().getAttribute("element");
 							Point centroidPointRight = gRight.getCentroid();
 							line = createLineString(centroidPoint, centroidPointRight);
-							gridMapMatrix[i][j].addLineString(new CustomLineString(line, 0));
+							this.gridMapMatrix[i][j].addLineString(new CustomLineString(line, 0,"RIGHT"));
 							
 						}
 						
 						if(i > 0) // if not most upper row
 						{
-							Geometry gUp = (Geometry)gridMapMatrix[i-1][j].getSquare().getAttribute("element");
+							Geometry gUp = (Geometry)this.gridMapMatrix[i-1][j].getSquare().getAttribute("element");
 							Point centroidPointUp = gUp.getCentroid();
 							line = createLineString(centroidPoint, centroidPointUp);
-							gridMapMatrix[i][j].addLineString(new CustomLineString(line, 0));
+							this.gridMapMatrix[i][j].addLineString(new CustomLineString(line, 0,"UP"));
 						}
 						if(i < (int)(matrixRows - 1)) // if not last row
 						{
-							Geometry gDown = (Geometry)gridMapMatrix[i+1][j].getSquare().getAttribute("element");
+							Geometry gDown = (Geometry)this.gridMapMatrix[i+1][j].getSquare().getAttribute("element");
 							Point centroidPointDown = gDown.getCentroid();
 							line = createLineString(centroidPoint, centroidPointDown);
-							gridMapMatrix[i][j].addLineString(new CustomLineString(line, 0));
+							this.gridMapMatrix[i][j].addLineString(new CustomLineString(line, 0,"DOWN"));
 						}
 						
 						
